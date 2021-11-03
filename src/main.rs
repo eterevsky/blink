e9527ebda8c22883d836160207a39801c5b92047
Oleg Eterevsky <oleg@eterevsky.com>
2021-11-03 21:18:08 +0100

    Trying to make USB work with Windows host.

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ use embedded_time::fixed_point::FixedPoint as _;
 use rp2040_hal as hal;
 use rp2040_hal::{clocks::Clock as _, pac, sio::Sio, watchdog::Watchdog};
 use usb_device;
-// use usb_device::bus::UsbBus as _;
+use usb_device::device::{UsbDeviceBuilder, UsbVidPid};
 
 #[link_section = ".boot2"]
 #[used]
@@ -40,8 +40,8 @@ fn main() -> ! {
     );
 
     let mut led_pin = pins.gpio25.into_push_pull_output();
-    let mut pin14 = pins.gpio14.into_push_pull_output();
-    let mut pin15 = pins.gpio15.into_push_pull_output();
+    let mut green_pin = pins.gpio16.into_push_pull_output();
+    let mut red_pin = pins.gpio17.into_push_pull_output();
 
     let clocks = hal::clocks::init_clocks_and_plls(
         XOSC_CRYSTAL_FREQ,
@@ -65,34 +65,77 @@ fn main() -> ! {
         &mut pac.RESETS,
     ));
 
+    // for _ in 0..5 {
+    //     led_pin.set_high().unwrap();
+    //     green_pin.set_high().unwrap();
+    //     red_pin.set_high().unwrap();
+    //     delay.delay_ms(500);
+    //     led_pin.set_low().unwrap();
+    //     green_pin.set_low().unwrap();
+    //     red_pin.set_low().unwrap();
+    //     delay.delay_ms(500);
+    // }
+
     let mut serial = usbd_serial::SerialPort::new(&usb_bus);
-
-    for _ in 0..5 {
-        led_pin.set_high().unwrap();
-        delay.delay_ms(500);
-        led_pin.set_low().unwrap();
-        delay.delay_ms(500);
-    }
-
-    match serial.write(b"Hello, World!\r\n") {
-        Ok(_) => { pin14.set_high().unwrap(); },
-        Err(_) => { pin15.set_high().unwrap(); }
-    };
+    let mut usb_dev = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0x2E8A, 0x000a))
+        .manufacturer("Raspberry Pi")
+        .product("Pico")
+        .serial_number("TEST")
+        .device_class(0xEF)
+        .device_sub_class(2)
+        .device_protocol(1)
+        .build();
 
     led_pin.set_high().unwrap();
 
-
+    let timer = hal::timer::Timer::new(pac.TIMER, &mut pac.RESETS);
+    let mut said_hello = false;
     loop {
-        serial.write(b"on\n").unwrap();
-        led_pin.set_high().unwrap();
-        pin14.set_high().unwrap();
-        pin15.set_low().unwrap();
-        delay.delay_ms(500);
+        // A welcome message at the beginning
+        if !said_hello && timer.get_counter() >= 2_000_000 {
+            said_hello = true;
+            // let _ = serial.write(b"Hello, World!\r\n");
+            match serial.write(b"Hello, World!\r\n") {
+                Ok(_) => { green_pin.set_high().unwrap(); },
+                Err(_) => { red_pin.set_high().unwrap(); }
+            };
+        }
 
-        serial.write(b"off\n").unwrap();
-        led_pin.set_low().unwrap();
-        pin14.set_low().unwrap();
-        pin15.set_high().unwrap();
-        delay.delay_ms(500);
+        // serial.write(b"on\n").unwrap();
+        // led_pin.set_high().unwrap();
+        // green_pin.set_high().unwrap();
+        // red_pin.set_low().unwrap();
+        // delay.delay_ms(500);
+
+        // serial.write(b"off\n").unwrap();
+        // led_pin.set_low().unwrap();
+        // green_pin.set_low().unwrap();
+        // red_pin.set_high().unwrap();
+        // delay.delay_ms(500);
+
+        if usb_dev.poll(&mut [&mut serial]) {
+            let mut buf = [0u8; 64];
+            match serial.read(&mut buf) {
+                Err(_e) => {
+                    // Do nothing
+                }
+                Ok(0) => {
+                    // Do nothing
+                }
+                Ok(count) => {
+                    // Convert to upper case
+                    buf.iter_mut().take(count).for_each(|b| {
+                        b.make_ascii_uppercase();
+                    });
+                    // Send back to the host
+                    let mut wr_ptr = &buf[..count];
+                    while !wr_ptr.is_empty() {
+                        let _ = serial.write(wr_ptr).map(|len| {
+                            wr_ptr = &wr_ptr[len..];
+                        });
+                    }
+                }
+            }
+        }
     }
 }
