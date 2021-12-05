7832e39a60ca63c74931047418a2a660c7d45501
Oleg Eterevsky <oleg@eterevsky.com>
2021-12-05 17:06:29 +0100

    Implement `core::fmt::Write` for USB console, making it possible to use it with `write!` macros.
    Panic handler now also uses the USB console

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,46 @@
-//! Blinks the LED on a Pico board
+//! Blinks the LED on a Pico board and log to USB console
 //!
 //! This will blink an LED attached to GP25, which is the pin the Pico uses for the on-board LED.
-//! It will also alternate the LEDs connected to GP14 and GP15.
+//! USB console can be used with write!() macro and is also used for panic! error message.
 #![no_std]
 #![no_main]
 
+use core::fmt::Write as _;
 use core::panic::PanicInfo;
 use embedded_hal::digital::v2::OutputPin;
 use embedded_time::fixed_point::FixedPoint as _;
 use rp2040_hal as hal;
-use rp2040_hal::pac::interrupt;
-use rp2040_hal::{clocks::Clock as _, pac, sio::Sio, watchdog::Watchdog};
+use rp2040_hal::{clocks::Clock as _, pac, pac::interrupt, sio::Sio, watchdog::Watchdog};
 use usb_device;
-use usb_device::{
-    bus::UsbBusAllocator,
-    device::{UsbDevice, UsbDeviceBuilder, UsbVidPid},
-};
-use usbd_serial::SerialPort;
+use usb_device::bus::UsbBusAllocator;
+
+mod usb_manager;
+
+use crate::usb_manager::UsbManager;
 
 #[link_section = ".boot2"]
 #[used]
 pub static BOOT2: [u8; 256] = rp2040_boot2::BOOT_LOADER_W25Q080;
 
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
+static mut USB_BUS: Option<UsbBusAllocator<hal::usb::UsbBus>> = None;
+static mut USB_MANAGER: Option<UsbManager> = None;
+
+#[allow(non_snake_case)]
+#[interrupt]
+unsafe fn USBCTRL_IRQ() {
+    match USB_MANAGER.as_mut() {
+        Some(manager) => manager.interrupt(),
+        None => (),
+    };
 }
 
-/// The USB Device Driver (shared with the interrupt).
-static mut USB_DEVICE: Option<UsbDevice<hal::usb::UsbBus>> = None;
-
-/// The USB Bus Driver (shared with the interrupt).
-static mut USB_BUS: Option<UsbBusAllocator<hal::usb::UsbBus>> = None;
-
-/// The USB Serial Device Driver (shared with the interrupt).
-static mut USB_SERIAL: Option<SerialPort<hal::usb::UsbBus>> = None;
+#[panic_handler]
+fn panic(panic_info: &PanicInfo) -> ! {
+    if let Some(usb) = unsafe { USB_MANAGER.as_mut() } {
+        writeln!(usb, "{}", panic_info).ok();
+    }
+    loop {}
+}
 
 // External high-speed crystal on the pico board is 12Mhz
 pub const XOSC_CRYSTAL_FREQ: u32 = 12_000_000;
@@ -45,13 +51,6 @@ fn main() -> ! {
     let core = pac::CorePeripherals::take().unwrap();
     let mut watchdog = Watchdog::new(pac.WATCHDOG);
     let sio = Sio::new(pac.SIO);
-
-    let pins = hal::gpio::Pins::new(
-        pac.IO_BANK0,
-        pac.PADS_BANK0,
-        sio.gpio_bank0,
-        &mut pac.RESETS,
-    );
 
     let clocks = hal::clocks::init_clocks_and_plls(
         XOSC_CRYSTAL_FREQ,
@@ -65,61 +64,37 @@ fn main() -> ! {
     .ok()
     .unwrap();
 
-    {
-        let usb_bus = UsbBusAllocator::new(hal::usb::UsbBus::new(
+    let usb = unsafe {
+        USB_BUS = Some(UsbBusAllocator::new(hal::usb::UsbBus::new(
             pac.USBCTRL_REGS,
             pac.USBCTRL_DPRAM,
             clocks.usb_clock,
             true,
             &mut pac.RESETS,
-        ));
-        unsafe {
-            USB_BUS = Some(usb_bus);
-        }
-    }
-    let usb_bus = unsafe { USB_BUS.as_mut().unwrap() };
-
-    {
-        let usb_serial = usbd_serial::SerialPort::new(usb_bus);
-        unsafe {
-            USB_SERIAL = Some(usb_serial);
-        }
-    }
-    let serial = unsafe { USB_SERIAL.as_mut().unwrap() };
-
-    let usb_dev = UsbDeviceBuilder::new(usb_bus, UsbVidPid(0x2E8A, 0x000a))
-        .manufacturer("Raspberry Pi")
-        .product("Pico")
-        .serial_number("TEST")
-        .device_class(2)
-        .device_protocol(1)
-        .build();
-    unsafe {
-        USB_DEVICE = Some(usb_dev);
-    }
-
-    // Enable the USB interrupt
-    unsafe {
+        )));
+        USB_MANAGER = Some(UsbManager::new(USB_BUS.as_ref().unwrap()));
+        // Enable the USB interrupt
         pac::NVIC::unmask(hal::pac::Interrupt::USBCTRL_IRQ);
+        USB_MANAGER.as_mut().unwrap()
     };
+
+    let pins = hal::gpio::Pins::new(
+        pac.IO_BANK0,
+        pac.PADS_BANK0,
+        sio.gpio_bank0,
+        &mut pac.RESETS,
+    );
 
     let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().integer());
     let mut led_pin = pins.gpio25.into_push_pull_output();
 
     loop {
         led_pin.set_high().unwrap();
-        serial.write(b"on\n").unwrap();
+        write!(usb, "On\n").unwrap();
         delay.delay_ms(500);
+
         led_pin.set_low().unwrap();
-        serial.write(b"off\n").unwrap();
+        write!(usb, "Off\n").unwrap();
         delay.delay_ms(500);
     }
-}
-
-#[allow(non_snake_case)]
-#[interrupt]
-unsafe fn USBCTRL_IRQ() {
-    let usb_dev = USB_DEVICE.as_mut().unwrap();
-    let serial = USB_SERIAL.as_mut().unwrap();
-    if usb_dev.poll(&mut [serial]) {}
 }
